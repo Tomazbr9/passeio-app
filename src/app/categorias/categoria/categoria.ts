601bc1797b32c47366164173269f44c56c02bfe4
Tomaz <tomazcraft@gmail.com>
2025-07-24 10:05:19 -0300

    foi implentado formularios reativos para criar categorias

--- src/app/categorias/categoria/categoria.ts
+++ src/app/categorias/categoria/categoria.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'; 
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.scss'
 })
 export class Categoria {
+  camposForm: FormGroup;
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required)
+    });
+  }
+
+  salvar(){
+    this.camposForm.markAllAsTouched();
+    if (this.camposForm.valid) {
+      console.log("Formulário válido. Enviando dados...");
+    }
+
+    console.log("Valores Digitados: ", this.camposForm.value);
+    console.log("Está valido? ", this.camposForm.valid);
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo?.touched && campo?.errors?.['required']
+  }
+
+
 
 }
